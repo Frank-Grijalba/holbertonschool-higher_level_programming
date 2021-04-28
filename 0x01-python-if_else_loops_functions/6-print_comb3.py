27e07549c93e54d5bc16f1030a5862033bee269c
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-04-27 19:29:57 -0500

    Exercises 5 to 8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 j = 0
 for i in range(10):
-    for j in range(i + 1, 10):
-        if i < 9 and j < 9:
+    for j in range (i + 1, 10):
+        if i < j and i != 8:
             print("{:d}{:d}".format(i, j), end=", ")
         elif i == 8 and j == 9:
             print("{:d}{:d}".format(i, j))
