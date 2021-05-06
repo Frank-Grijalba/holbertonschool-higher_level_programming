8e213d506f34c9596a6b7607dbbf9ee711f86be4
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-05 22:20:05 -0500

    Exercises 9 to 12

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    temp = my_list[0]
     if my_list == []:
         return None
+    temp = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > temp:
             temp = my_list[i]
