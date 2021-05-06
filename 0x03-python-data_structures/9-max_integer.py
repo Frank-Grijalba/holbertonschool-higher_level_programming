8adada78d85c7f799bfe3c9206b41a6b69205838
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-05 22:08:42 -0500

    Exercises 9 to 12

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    temp = 0
+    if my_list is None:
+        return None
+
+    for i in range(len(my_list)):
+        if my_list[i] > temp:
+            temp = my_list[i]
+
+    return temp
