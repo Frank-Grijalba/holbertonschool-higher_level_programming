ad5b7d8cc58d7f5ceb3d112cf6905b480155dc89
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-18 15:48:17 -0500

    Exercises 0 and 1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """class Square"""
+    def __init__(self):
+        """Square class init"""
+        return None
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """class Square
+       __size (int): The private variable.
+    """
+    __size = None
+
+    def __init__(self, size):
+        """Square class init
+           _asign a value to variable __size """
+        self.__size = size
