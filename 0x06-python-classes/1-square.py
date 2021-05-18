52fa86a2af230cb4f4338da908c7662833d0897d
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-18 15:59:18 -0500

    Exercises 0 and 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-class Square:
+class Square():
     """class Square"""
-
     def __init__(self, size):
         """Square class init, assign a value to priv var __size """
         self.__size = size
