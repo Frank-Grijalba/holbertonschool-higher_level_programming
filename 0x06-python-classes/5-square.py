62fddc7f128879dc18a7322aa6aef6be2acf6482
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-18 19:44:17 -0500

    Exercise 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """4-square
-a class Square that defines a square by: (based on 3-square.py)"""
+a class Square that defines a square by: (based on 4-square.py)"""
 
 
 class Square:
@@ -8,10 +8,10 @@ class Square:
     def __init__(self, size=0):
         """Square class init, assign a value to
         Args:
-            value (int): The private arg size of the square.
+            size (int): The private arg size of the square.
         Raise:
-            ValueError: If value is less than Zero
-            TypeError: If value is not an integer
+            ValueError: If size is less than Zero
+            TypeError: If size is not an integer
         """
         if size < 0:
             raise ValueError("size must be >= 0")
@@ -38,13 +38,21 @@ class Square:
     def size(self, value):
         """Method setter: set the size
         Args:
-            size (int): The private arg size of the square.
+            value (int): The private arg size of the square.
         Raise:
-            ValueError: If size is less than Zero
-            TypeError: If size is not an integer
+            ValueError: If value is less than Zero
+            TypeError: If value is not an integer
         """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def my_print(self):
+        """ Method that prints in stdout the square with the character"""
+        if self.__size == 0:
+            print("\n")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
