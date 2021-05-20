0b10be356b7d97d8bc77dc5e9a639ec5d69071f5
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-20 18:53:08 -0500

    Exercises for checker

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -48,4 +48,4 @@ class Rectangle:
         """Calculate perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width * self.__height) * 2
+        return (self.__width * 2 + self.__height * 2)
