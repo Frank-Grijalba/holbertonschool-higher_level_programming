92f157149e6ba76631b1bad66abd926387a2f7ec
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-21 20:25:49 -0500

    All ex

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(repeat=[-1]):
+    repeat[0] += 1
+    return "Holberton, " * repeat[0] + "Holberton"
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def copy_list(l):
+    n = []
+    n = l[:]
+    return n
