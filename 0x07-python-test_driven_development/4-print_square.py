d8dafd37ee7a909ad6568d87ced9349dce97398f
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-05-26 20:48:00 -0500

    Exercise 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+>>> print_square(4)
+####
+####
+####
+####
+
+"""
+
+
+def print_square(size):
+    str = ""
+    if (type(size) is float and size < 0) or type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        str = str + ("#" * size) + "\n"
+    print(str[:-1])
