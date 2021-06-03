8874619b45109db78bbfa60fe4c0ecff837a8b97
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-06-02 20:35:01 -0500

    Exercise 0 - 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,14 +5,14 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == "__main__":
     import sys
-    
+
     args = sys.argv[1:]
     my_list = []
-    
+
     try:
         my_list = load_from_json_file("add_item.json")
     except Exception:
         my_list = []
-    
+
     my_list.extend(args)
     save_to_json_file(my_list, "add_item.json")
