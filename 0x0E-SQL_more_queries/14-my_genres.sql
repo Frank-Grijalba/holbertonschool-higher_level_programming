db9731aca7604e698f745b3b650ccb4011edcea9
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-07-14 09:24:52 -0500

    Fix 0, 7 - add task 100

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
-SELECT DISTINCT tv_genres.name
+SELECT tv_genres.name
 FROM tv_genres, tv_shows, tv_show_genres
 WHERE tv_genres.id=tv_show_genres.genre_id
 AND tv_show_genres.show_id = tv_shows.id 
