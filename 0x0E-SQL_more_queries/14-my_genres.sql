b489d14531fcf59ac17732d97e3620e7ccbf8b6c
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-07-13 21:47:46 -0500

    Tasks 14-16)

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+SELECT DISTINCT tv_genres.name
+FROM tv_genres, tv_shows, tv_show_genres
+WHERE tv_genres.id=tv_show_genres.genre_id
+AND tv_show_genres.show_id = tv_shows.id 
+AND tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,5 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+SELECT tv_shows.title FROM tv_genres, tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+AND tv_show_genres.genre_id = tv_genres.id
+AND tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC;
