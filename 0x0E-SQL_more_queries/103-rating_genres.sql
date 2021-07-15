156c88cf2c9ea20a2357a692bbeab9501f849629
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-07-14 22:15:43 -0500

    Add tasks 18, 19 and 20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,4 +4,4 @@ FROM tv_genres, tv_show_ratings, tv_show_genres
 WHERE tv_genres.id = tv_show_genres.genre_id
 AND tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name 
-ORDER BY SUM(tv_show_ratings.rate) DESC;
+ORDER BY rating DESC;
