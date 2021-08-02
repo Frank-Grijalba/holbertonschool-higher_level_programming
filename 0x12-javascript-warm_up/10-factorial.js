6e63b3cfa93ec16b1d6c81f305d7b80b9eeb668a
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-08-02 17:16:42 -0500

    fix task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const argv = process.argv;
+const number = parseInt(argv[2]);
+
+if (isNaN(number)) {
+  console.log(1);
+} else {
+  const factorial = (n) => {
+    if (n <= 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  };
+  console.log(factorial(number));
+}
