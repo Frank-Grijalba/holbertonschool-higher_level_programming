09d2e00acc6e168662ca80f9d93b4ba9df8ae8d9
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-08-02 13:43:01 -0500

    fix 0-2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,13 @@
 const argv = process.argv.length;
 
 if (argv < 3) {
-    console.log('No argument');
+    console.log('No argument')
 }
 else if (argv === 3)
 {
-    console.log('Argument found');
+    console.log('Argument found')
 }
 else
 {
-    console.log('Arguments found');
+    console.log('Arguments found')
 }
