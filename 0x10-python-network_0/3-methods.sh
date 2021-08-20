187171902644c11d5a05f7d7968de33b1d29886b
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-08-19 19:51:21 -0500

    Tasks 0-5

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" -X POST | grep Allow |cut -d':' -f2 | sed 's/^[[:space:]]*//' 
-
+curl -sI "$1" -X POST | grep Allow |cut -d':' -f2 | sed 's/^[[:space:]]*//'
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # a Bash script that get the method and show the response
 curl "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
-
