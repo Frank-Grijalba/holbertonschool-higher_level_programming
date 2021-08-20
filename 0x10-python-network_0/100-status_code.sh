5f46150473be3350f542f351451a8da16b5d61ea
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-08-19 21:38:07 -0500

    change comments

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-# not allowed to use any pipe, redirection, not allowed to use ; and &&
 curl -sI "$1" -w '%{http_code}' -o /dev/null
