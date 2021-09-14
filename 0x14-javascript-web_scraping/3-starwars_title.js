61830ec95238778511609d859c958d9d98e6a50c
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-09-14 16:06:30 -0500

    fix tasks 2-3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-//script that prints the title of a Star Wars movie where the
-//episode number matches a given integer.
+// script that prints the title of a Star Wars movie where the
+// episode number matches a given integer.
 const request = require('request');
 
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
